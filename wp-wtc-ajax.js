09a0dea2e452e6b5ef69a9394131403cfb212612
aviaxis <aviaxis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-16 12:25:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/traffic-counter-widget/trunk@424184 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-wtc-ajax.js
@@ -0,0 +1,27 @@
+var xmlhttp;
+if (window.XMLHttpRequest)
+  xmlhttp=new XMLHttpRequest();
+else
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+
+function wtc_show (stat, plugin_url, blog_url) {
+
+  document.getElementById("wtc_stats_title").innerHTML = '<img src="'+plugin_url+'" title="Loading Stats" alt="Loading Stats" border="0">';
+  xmlhttp.onreadystatechange=wtc_change_stat;
+  xmlhttp.open("GET",blog_url+"/wp-admin/admin-ajax.php?action=wtcstats&reqstats="+stat,true);
+  xmlhttp.send(); 
+}
+
+function wtc_change_stat () {
+
+  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+
+     var rt = xmlhttp.responseText;
+     var wtcdata = rt.split(',');
+     document.getElementById("wtc_stats_title").innerHTML = wtcdata[0];
+     document.getElementById("wtc_lds").innerHTML = wtcdata[1];
+     document.getElementById("wtc_lws").innerHTML = wtcdata[2];
+     document.getElementById("wtc_lms").innerHTML = wtcdata[3];
+
+  }
+}
